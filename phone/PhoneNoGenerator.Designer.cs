ae6b16a92a14f432504f883a1d8bdee08f615463
sniper <sdqdtxl@gmail.com>
2019-07-17 19:50:46 +0800

    V1.0

---
+++ phone/PhoneNoGenerator.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace phone
+{
+    partial class FORM1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.generateBtn = new System.Windows.Forms.Button();
+            this.oprBox = new System.Windows.Forms.ComboBox();
+            this.provinceBox = new System.Windows.Forms.ComboBox();
+            this.cityBox = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "公司名称";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "省或行政区";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(246, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "城市";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "数量";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // generateBtn
+            // 
+            this.generateBtn.Location = new System.Drawing.Point(324, 40);
+            this.generateBtn.Name = "generateBtn";
+            this.generateBtn.Size = new System.Drawing.Size(75, 23);
+            this.generateBtn.TabIndex = 8;
+            this.generateBtn.Text = "导出";
+            this.generateBtn.UseVisualStyleBackColor = true;
+            this.generateBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // oprBox
+            // 
+            this.oprBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.oprBox.FormattingEnabled = true;
+            this.oprBox.Location = new System.Drawing.Point(135, 22);
+            this.oprBox.Name = "oprBox";
+            this.oprBox.Size = new System.Drawing.Size(121, 21);
+            this.oprBox.TabIndex = 9;
+            this.oprBox.SelectedIndexChanged += new System.EventHandler(this.oprBox_SelectedIndexChanged);
+            // 
+            // provinceBox
+            // 
+            this.provinceBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.provinceBox.FormattingEnabled = true;
+            this.provinceBox.Location = new System.Drawing.Point(135, 98);
+            this.provinceBox.Name = "provinceBox";
+            this.provinceBox.Size = new System.Drawing.Size(64, 21);
+            this.provinceBox.TabIndex = 10;
+            this.provinceBox.Visible = false;
+            this.provinceBox.SelectedIndexChanged += new System.EventHandler(this.provinceBox_SelectedIndexChanged);
+            // 
+            // cityBox
+            // 
+            this.cityBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cityBox.FormattingEnabled = true;
+            this.cityBox.Location = new System.Drawing.Point(301, 98);
+            this.cityBox.Name = "cityBox";
+            this.cityBox.Size = new System.Drawing.Size(64, 21);
+            this.cityBox.TabIndex = 11;
+            this.cityBox.Visible = false;
+            this.cityBox.SelectedIndexChanged += new System.EventHandler(this.cityBox_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(41, 127);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(158, 264);
+            this.listBox1.TabIndex = 12;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(249, 127);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox2.Size = new System.Drawing.Size(160, 264);
+            this.listBox2.TabIndex = 13;
+            // 
+            // FORM1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 403);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.cityBox);
+            this.Controls.Add(this.provinceBox);
+            this.Controls.Add(this.oprBox);
+            this.Controls.Add(this.generateBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FORM1";
+            this.Text = "客户关系管理";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button generateBtn;
+        private System.Windows.Forms.ComboBox oprBox;
+        private System.Windows.Forms.ComboBox provinceBox;
+        private System.Windows.Forms.ComboBox cityBox;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ phone/PhoneNoGenerator.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace phone
+{
+    public partial class FORM1 : Form
+    {
+        private String oprName=null;
+        private String province;
+        private String city;
+        private String count=null;
+        private int iAmount;
+        private int sqlAmount;
+        private string[] provinceList= { "北京", "天津", "上海", "广东", "山东", "江苏", "浙江", "安徽", "四川", "陕西", "湖北", "广西", "河南", "甘肃", "吉林", "辽宁", "内蒙古", "新疆", "黑龙江", "福建", "河北", "重庆", "海南", "江西", "山西", "湖南", "青海", "贵州", "宁夏", "云南", "西藏" };
+        private string whereProvince;
+        private string whereNotCity;
+        private string whereCity;
+        SQLiteConnection conn = null;
+        private int PER_PRE7_MAX = 10000;
+        string dbPath = "Data Source =" + Environment.CurrentDirectory + "/phone.db";
+        public FORM1()
+        {
+            InitializeComponent();
+
+            oprBox_Load();
+
+           //cityBox_Load();
+
+            provinceBox_Load();
+
+            listBoxLoad();
+
+            conn = new SQLiteConnection(dbPath);//创建数据库实例，指定文件位置  
+            conn.Open();//打开数据库，若文件不存在会自动创建 
+
+
+        }
+
+        private void oprBox_Load()
+        {
+            this.oprBox.Items.Add("Y");
+            this.oprBox.Items.Add("L");
+            this.oprBox.Items.Add("D");
+        }
+
+        private void provinceBox_Load()
+        {
+            for (int i=0; i<31; i++)
+            {
+                this.provinceBox.Items.Add(provinceList[i]);
+            }
+        }
+
+        private void listBoxLoad()
+        {
+            for (int i = 0; i < 31; i++)
+            {
+                this.listBox1.Items.Add(provinceList[i]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*TODO:
+             * √ 1、改为去掉哪些省市
+             * √ 2、点击导出时要求输入秘钥
+             * √ 3、多省市的混合输出
+             * √ 4、生成的文件名称为 年月日时分秒.txt
+             * */
+            int sum = 0;
+            int i = 0;
+            string sqlDetail;
+            int[] suffix4 = new int[PER_PRE7_MAX];
+            int temp;
+            int t;
+            int indexPre7;
+            string[] resultList = new string[440000];
+            string sqlCount1;
+            string sqlCount2;
+            for ( i = 0; i < PER_PRE7_MAX; i++)
+            {
+                suffix4[i] = i;
+            }
+
+            //密码校验
+            string passwd = Interaction.InputBox("请输入软件密码，否则无法继续进行", "操作验证", "****************", -1, -1);
+            if (!"1Gl83gz".Equals(passwd))
+            {
+                this.Close();
+            }
+
+            count = textBox1.Text.ToString();
+            int iCount = int.Parse(count);
+
+            whereNotCity = "(";
+            whereCity = "(";
+            for (int x = 0; x < listBox2.Items.Count; x++)
+            {
+                if (listBox2.GetSelected(x)) {
+                    whereNotCity += ("'" + listBox2.Items[x].ToString() + "',");
+                } else {
+                    whereCity+= ("'" + listBox2.Items[x].ToString() + "',");
+                }
+            }
+            whereNotCity = whereNotCity.Substring(0, whereNotCity.Length - 1) + ")";
+            whereCity = whereCity.Substring(0, whereCity.Length - 1) + ")";
+            //查询数据,且给sqlAmount赋值
+            if (null == oprName) {
+                sqlCount1 = "select count(*) from phone where city in" + whereCity;
+                sqlCount2 = "select count(*) from phone where province not in " + whereProvince;
+            } else {
+                sqlCount1 = "select count(*) from phone where isp='" + oprName + "' and city in" + whereCity;
+                sqlCount2 = "select count(*) from phone where isp='" + oprName + "' and province not in " + whereProvince;
+            }
+
+            SQLiteCommand cmdC1 = new SQLiteCommand();
+            cmdC1.CommandText = sqlCount1;
+            cmdC1.Connection = conn;
+            int sqlAmount = Convert.ToInt32(cmdC1.ExecuteScalar());
+
+            SQLiteCommand cmdC2 = new SQLiteCommand();
+            cmdC2.CommandText = sqlCount2;
+            cmdC2.Connection = conn;
+            sqlAmount += +Convert.ToInt32(cmdC2.ExecuteScalar());
+
+            //需要生成iCount/10000+1次
+            iAmount = iCount / 10001 + 1;
+            //需要生成的号段数量和数据库中的号段数量对比
+            if (iAmount > sqlAmount)
+            {
+                MessageBox.Show("选中地区数据量不足");
+                return;
+            }
+
+            if (null == oprName) {
+                sqlDetail = "select * from phone where city in" + whereCity
+                    +"union select * from phone where province not in " + whereProvince;
+            } else {
+                sqlDetail = "select * from phone where isp='" + oprName + "' and city in" + whereCity
+                    + "union select * from phone where province not in " + whereProvince;
+            }
+            SQLiteCommand cmdQ = new SQLiteCommand(sqlDetail, conn);
+            SQLiteDataReader reader = cmdQ.ExecuteReader();
+
+            string fileName = DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt";
+            FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(fs);
+                       
+            for (i=0; reader.Read(); i++)
+            {
+                resultList[i] = reader[1].ToString();
+            }
+
+            i = 0;
+            Random pre7Radom = new Random();
+            Random suf4Radom = new Random();
+            while ( i < (iCount / 10001 + 1))
+            {
+                for (int j=0; j< PER_PRE7_MAX && sum < iCount; j++)
+                {
+                    t = suf4Radom.Next(PER_PRE7_MAX-j)+j;
+                    temp = suffix4[j];
+                    suffix4[j] = suffix4[t];
+                    suffix4[t] = temp;
+
+                    indexPre7 = pre7Radom.Next(sqlAmount);
+
+                    sw.WriteLine(resultList[indexPre7] + String.Format("{0,-10:D4}", suffix4[j]));
+                    sum++;
+                }
+                i++;
+            }
+            //while (reader.Read() && i < (iCount / 10001 + 1))
+            //{
+            //    //开始生成(前7位决定地区和运营商，只需按顺序生成后四位)
+            //    for (int j = 0; j < (iCount<10000?iCount:10000); j++)
+            //    {
+            //        sw.WriteLine(reader[1].ToString() + String.Format("{0,-10:D4}", j));   //写入字符串
+            //        sum++;
+            //    }
+            //    i++;
+            //}
+            
+            MessageBox.Show("数据共计" + sum.ToString()+"！");
+            sw.Close();
+            conn.Close();
+        }
+
+        private void oprBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            oprName = this.oprBox.SelectedItem.ToString();
+            if ("Y".Equals(oprName)) {
+                oprName = "移动";
+            } else if ("L".Equals(oprName)){
+                oprName = "联通";
+            }else {
+                oprName = "电信";
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string sqlDetail;
+            whereProvince = "(";
+            for (int x = 0; x < listBox1.Items.Count; x++)
+            {
+                if (listBox1.GetSelected(x))
+                {
+                    whereProvince += ("'" + listBox1.Items[x].ToString() + "',");
+                }
+            }
+            whereProvince = whereProvince.Substring(0, whereProvince.Length - 1) + ")";
+            this.listBox2.Items.Clear();
+
+            if(null == oprName) {
+                sqlDetail = "select distinct city from phone where province in" + whereProvince;
+            } else {
+                sqlDetail = "select distinct city from phone where isp='" + oprName + "' and province in" + whereProvince;
+            }
+            SQLiteCommand cmdCity = new SQLiteCommand(sqlDetail, conn);
+            SQLiteDataReader cityReader = cmdCity.ExecuteReader();
+            while (cityReader.Read())
+            {
+                this.listBox2.Items.Add(cityReader[0].ToString());
+            }
+        }
+
+
+        private void provinceBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            province = this.provinceBox.SelectedItem.ToString();
+            this.cityBox.Items.Clear();
+
+            //查询数据,且给sqlAmount赋值
+            string sqlDetail = "select distinct city from phone where isp='" + oprName + "' and province =" + province;
+            SQLiteCommand cmdCity = new SQLiteCommand(sqlDetail, conn);
+            SQLiteDataReader cityReader = cmdCity.ExecuteReader();
+            while (cityReader.Read())
+            {
+                this.cityBox.Items.Add(cityReader[0].ToString());
+            }
+            conn.Close();
+        }
+
+        private void cityBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            city = this.cityBox.SelectedItem.ToString();
+        }
+
+        //private void textBox1_TextChanged(object sender, EventArgs e)
+        //{
+
+        //}
+    }
+}
